4b2d6bde0975e3ae77d2dff3b4f803d843dffb49
David Gonzalez <dgonzalez@opentrends.net>
2018-09-08 19:40:49 +0200

    Added ResponseTextFile

---
+++ src/test/java/ApiTesting.java
@@ -0,0 +1,11 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+
+public class ApiTesting {
+    
+
+}
