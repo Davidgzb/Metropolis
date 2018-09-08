bc4b9edc022c083883a38bde4231106080eb90ef
David Gonzalez <dgonzalez@opentrends.net>
2018-09-08 20:17:14 +0200

    Reading txt File

--- src/test/java/ApiTesting.java
+++ src/test/java/ApiTesting.java
@@ -4,8 +4,43 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class ApiTesting {
-    
+
+    @Test
+    public void checkJsonStructure(){
+
+        RestAssured.baseURI = "https://api.github.com/repos/metrolab/SingleDateAndTimePicker";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.request(Method.GET);
+
+        String responseBody = response.getBody().asString();
+        System.out.println("Response body is => " + responseBody);
+
+        String fileName = "ResponseTextFile.txt";
+        String line = null;
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File Not Found");
+        } catch (IOException e) {
+            System.out.println("Error reading the file");
+        }
+
+    }
 
 }
